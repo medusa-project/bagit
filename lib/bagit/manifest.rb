5e8d210c90088f3abc15b3cafe1d914c1312fd7a
Howard Ding <hding2@illinois.edu>
2015-10-12 12:06:53 -0500

    fix typo

--- lib/bagit/manifest.rb
+++ lib/bagit/manifest.rb
@@ -33,7 +33,7 @@ module BagIt
         begin
           rel_path = Pathname.new(f).relative_path_from(Pathname.new(bag_dir)).to_s
         rescue Exception => e
-          if f.starts_with?(bag_dir)
+          if f.start_with?(bag_dir)
             rel_path = f.sub(/^#{bag_dir}/, '').to_s
           else
             raise
